43b38eaf07daabaf88bef568fe7ab10c8af9ef6d
rudeuscapital <rudeuscapital@gmail.com>
2025-12-07 19:20:59 +0700

    a

--- src/components/molecules/Footer.svelte
+++ src/components/molecules/Footer.svelte
@@ -8,7 +8,7 @@
 <footer class="wrapper">
 	<Socials />
 	<h6>
-	develops by @GNON Labs | △
+	develops by @slade
 		<Tooltip tip="how to delete">
 			<a href="" target="_blank" rel="noreferrer"></a>
 		</Tooltip>
