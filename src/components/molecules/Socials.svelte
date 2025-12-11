75d66befd71cbbba103109c60b90e78305afb317
rudeuscapital <rudeuscapital@gmail.com>
2025-12-12 01:52:51 +0700

    a

--- src/components/molecules/Socials.svelte
+++ src/components/molecules/Socials.svelte
@@ -36,7 +36,7 @@
 	</Social>
 
 	<!-- Pumpfun / Dexscreener -->
-	<Social tip="$Leafinite" link="Btn2JhzT8UgHK3DaK5Ksa3ZCNEBmSFAVCcQzLD8apump">
+	<Social tip="$Leafinite" link="https://pump.fun/coin/Btn2JhzT8UgHK3DaK5Ksa3ZCNEBmSFAVCcQzLD8apump">
 		<img src="/dexscreener.svg" alt="$Leafinite" class="icon icon-img" />
 	</Social>
 </div>
